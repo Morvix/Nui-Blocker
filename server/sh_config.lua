ce6db324a75db24102e7fc72d524c96f43796640
MORVIX <149473430+Morvix@users.noreply.github.com>
2024-07-10 18:07:56 +0530

    Update sh_config.lua

--- server/sh_config.lua
+++ server/sh_config.lua
@@ -3,5 +3,5 @@ SConfig = {}
 SConfig.CoreName = 'qb-core' -- Core Folder Name
 SConfig.BanPlayer = false     -- Ban Player if Nui Dev Tool Opened if false it will kick player
 SConfig.Log = true           -- Should Give you Log
-SConfig.LogWebhook = 'https://discord.com/api/webhooks/1155993682797940797/9yDbZV21_0IdgDV8P8VL3HloHSBGpAwCOTEO6K_hsMq85UlPF6nfogM8NfSJnkKC9Ke6'      -- Discord Webhook for Log
+SConfig.LogWebhook = ''      -- Discord Webhook for Log
 SConfig.Rreason = "WaveShield Player Suspected"
